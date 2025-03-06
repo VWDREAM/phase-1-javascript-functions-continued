2744528cdd0c965b52f76f86a8300e5c063622ce
VWDREAM <wanjiruvirginiah26@gmail.com>
2025-03-06 12:44:01 +0300

    my function lab 2

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const { expect } = require("chai");
+
 // code your solution here
+function saturdayFun( activity ='roller-skate'){
+return `This Saturday, I want to ${activity}!`;
+
+}
+console.log(saturdayFun);
+
+function mondayWork(activity='go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork(`work from home`));
+
+function wrapAdjective(wrapper = "*") {
+    return function(adjective) {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+}
